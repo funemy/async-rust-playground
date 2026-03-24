3e22749fe5e305790d2b2be2b3015186e8858322
funemy <liyzunique@gmail.com>
2026-03-24 14:18:59 -0700

    minor

--- mir.sh
+++ mir.sh
@@ -3,6 +3,6 @@
 # `unpretty` is one of:
 # `normal`, `identified`, `expanded`, `expanded,identified`, `expanded,hygiene`, `ast-tree`, `ast-tree,expanded`, `hir`, `hir,identified`, `hir,typed`, `hir-tree`, `thir-tree`, `thir-flat`, `mir` or `mir-cfg`
 # cargo rustc --lib $1 -- -Z unpretty=hir,typed
-cargo +nightly rustc --bin $1 -- -Z unpretty=hir,typed
+cargo +nightly rustc --bin $1 -- -Z unpretty=mir-cfg
 # if you'd like a tree structure
 # cargo +nightly rustc --bin hello_world -- -Z unpretty=hir-tree
